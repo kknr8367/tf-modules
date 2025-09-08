31b6fda8ce9b85faa90d916db07ef2c80941bdaf
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-08 21:44:03 +0530

    1 commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source              = "./modules/vpc"
+  source              = "./vpc"
   vpc_name            = "my-vpc"
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
