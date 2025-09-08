8d1ad79d6d8847b9feb948e2cfc6930b7d961771
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-08 17:12:51 +0530

    29th commit

--- rds/main.tf
+++ rds/main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "main" {
   allocated_storage    = var.allocated_storage
   engine               = var.engine
   instance_class       = var.db_instance_class
-  name                 = var.db_name
+  #name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   db_subnet_group_name = var.subnet_group
